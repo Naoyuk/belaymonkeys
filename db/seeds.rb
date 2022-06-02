276b5d64b06353e583d8981de693bfca263269e2
Naoyuki Ishida <39.ishida@gmail.com>
2022-06-02 13:40:53 -0700

    Implement basic posts list

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+3.times do |n|
+  User.create!(
+    email: "test#{n}@example.com",
+    password: "password",
+    password_confirmation: "password"
+  )
+end
+
+10.times do |n|
+  Post.create!(
+    date: Date.new.strftime('%Y-%m-%d'),
+    start_time: Time.new.strftime('%H:%M'),
+    end_time: Time.new.strftime('%H:%M'), 
+    kind_of_climbing: 0,
+    description: "test#{n}#",
+    user_id: User.first.id
+  )
+end
