a1a791103e699d026d17a7a46d33659d4dfb949a
Naoyuki Ishida <39.ishida@gmail.com>
2022-06-02 01:14:10 -0700

    Implement devise and post resources

---
+++ db/migrate/20220602076031_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.timestamp :date
+      t.time :start_time
+      t.time :end_time
+      t.integer :kind_of_climbing
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
