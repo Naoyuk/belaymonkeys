92e950a2a95089fb7da8d6d0160e462b62913978
Naoyuki Ishida <39.ishida@gmail.com>
2022-06-28 10:33:59 -0700

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,18 @@ end
 
 # Seeds for posts
 areas = ['Smoke Bluffs', 'Grandwall', 'Murrin Park', 'Cheakamus Canyon']
+descriptions = [
+  'I am looking for a partner for next Monday. Shall we go to some trad?',
+  'Hi, I am visiting in Squamish for a month. Please rope up with me. I would like to sport.',
+  'I usually climb 5.9 to 5.10s on trad. I can go climbing only weekend. Thank you.',
+  'Hello, guys. Please teach me climbing. I am a beginner.',
+  'My name is John. My favorite climb is bouldering. Who want to go with me?',
+  'I found a big project. It seems like 5.14. Who wants FA with me?',
+  'Need a partner on this Friday! I climb 5.11 to 5.12 sports.',
+  'I found a pair of climbing shoes at somewher in Area44. Message me with the size and model.',
+  'I have just moved to Squamish 1 week ago. Please take me to the crag.',
+  'My partner is injured. So I need another partner for this season. Message me.'
+]
 
 10.times do |n|
   Post.create!(
@@ -28,7 +40,7 @@ areas = ['Smoke Bluffs', 'Grandwall', 'Murrin Park', 'Cheakamus Canyon']
     end_time: Time.new.strftime('%H:%M'), 
     area: areas[rand(4)],
     kind_of_climbing: 0,
-    description: "test#{n}#",
+    description: descriptions[n],
     user_id: User.find(rand(3) + 1).id
   )
 end
