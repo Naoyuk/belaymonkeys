734d34db2fed46740f802eadfbc65ed6f4571a1b
Naoyuki Ishida <39.ishida@gmail.com>
2022-07-10 13:41:21 -0700

    Implement ChatsHelper specs completely

--- spec/helpers/chats_helper_spec.rb
+++ spec/helpers/chats_helper_spec.rb
@@ -34,10 +34,37 @@ RSpec.describe ChatsHelper, type: :helper do
   end
 
   describe 'chat_id_by_ids' do
-
+    before do
+      @chat = FactoryBot.create(:chat)
+      @post = @chat.post_id
+      @host = @chat.host_user_id
+      @guest = @chat.guest_user_id
+    end
+    it 'returns chat id with proper post_id, host_user_id and guest_user_id' do
+      chat_id = chat_id_by_ids(post_id: @post, host_user_id: @host, guest_user_id: @guest)
+      expect(chat_id).to eq @chat.id
+    end
+    it 'returns false with wrong ids' do
+      chat_id = chat_id_by_ids(post_id: -1, host_user_id: @host, guest_user_id: @guest)
+      expect(chat_id).to eq false
+    end
+    it 'returns false without any of post_id, host_user_id or guest_use_id' do
+      chat_id = chat_id_by_ids(post_id: nil, host_user_id: @host, guest_user_id: @guest)
+      expect(chat_id).to eq false
+      chat_id = chat_id_by_ids(post_id: @post, host_user_id: nil, guest_user_id: @guest)
+      expect(chat_id).to eq false
+      chat_id = chat_id_by_ids(post_id: @post, host_user_id: @host, guest_user_id: nil)
+      expect(chat_id).to eq false
+    end
   end
 
   describe 'message_class' do
+    context 'when user logged in' do
+      it 'returns class with mb-2, p-2, rounded-md, bg-blue-400 and text-white'
+    end
 
+    context 'when user not logged in' do
+      it 'returns class with mb-2, p-2, rounded-md, bg-gray-200'
+    end
   end
 end
