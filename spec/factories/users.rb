dae6923a38624762f8bd7a79c801bb94547dde93
Naoyuki Ishida <39.ishida@gmail.com>
2022-07-13 23:37:51 -0700

    Implement create new post

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@
 
 FactoryBot.define do
   factory :user, aliases: [:host, :guest] do
+    sequence(:name) { |n| "test#{n}" }
     sequence(:email) { |n| "test#{n}@example.com" }
     password { '123456' }
     password_confirmation { '123456' }
