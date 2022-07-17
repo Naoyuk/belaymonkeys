67737f672396d84fb036588e43696b8d6f88f32a
Naoyuki Ishida <39.ishida@gmail.com>
2022-07-16 18:37:02 -0700

    Add posts system specs

---
+++ spec/support/post_create_support.rb
@@ -0,0 +1,13 @@
+module PostCreateSupport
+  def create_post(description)
+    click_link 'New Post'
+    fill_in 'post[date]', with: 1.day.before
+    fill_in 'post[description]', with: description
+    fill_in 'post[area]', with: 'Grandwall'
+    click_button 'Create Post'
+  end
+end
+
+RSpec.configure do |config|
+  config.include PostCreateSupport
+end
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -17,14 +17,87 @@ RSpec.describe 'Posts', type: :system do
     click_button 'Log in'
 
     expect do
-      click_link 'New Post'
-      fill_in 'post[date]', with: 1.day.before
-      fill_in 'post[description]', with: 'test text'
-      fill_in 'post[area]', with: 'Grandwall'
-      click_button 'Create Post'
+      description = 'test post'
+      create_post(description)
 
       expect(page).to have_content 'New post was successfully created.'
-      expect(page).to have_content 'test text'
+      expect(page).to have_content description
     end.to change(user.posts, :count).by(1)
+  end
+
+  describe 'posts have a button that shows chat' do
+    let(:host) { FactoryBot.create(:host) }
+    let(:guest) { FactoryBot.create(:guest) }
+
+    context 'when a post that is own post' do
+      scenario 'post shows "See Conversions" button and link to related chats' do
+        # Log in as a host
+        sign_in_as host
+
+        # A host creates a post
+        description = 'own post'
+        create_post(description)
+        visit root_path
+
+        expect(page).to have_content description
+        expect(page).to have_content 'See Conversations'
+
+        # A host see conversations
+        click_link 'See Conversations'
+        
+        expect(page).to have_content "No messages."
+        
+        # A host log out and a guest log in
+        click_link 'Log out'
+        sign_in_as guest
+        visit root_path
+
+        # a guest send a message
+        click_link 'Message'
+        msg = 'test message from guest'
+        fill_in 'message[body]', with: msg
+        click_button 'Send Message'
+        expect(page).to have_content msg
+        
+        # A guest log out and a host log in
+        click_link 'Log out'
+        sign_in_as host
+        visit root_path
+        
+        # A host see conversations
+        click_link 'See Conversations'
+        
+        expect(page).to have_content msg
+      end
+    end
+
+    context 'when a post that is not own' do
+      scenario 'shows "Message" button and link to the chat' do
+        # Log in as a host
+        sign_in_as host
+
+        # A host creates a post
+        description = 'other\'s post'
+        create_post(description)
+        visit root_path
+
+        # A host log out and a guest log in
+        click_link 'Log out'
+        sign_in_as guest
+        visit root_path
+
+        expect(page).to have_content 'Message'
+
+        # a guest send a message
+        click_link 'Message'
+        
+        expect(page).to have_content "Conversation with: #{host.name}"
+
+        msg = 'test message from guest'
+        fill_in 'message[body]', with: msg
+        click_button 'Send Message'
+        expect(page).to have_content msg
+      end
+    end
   end
 end
