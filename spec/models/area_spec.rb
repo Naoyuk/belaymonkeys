96b268f8db4f552a2ca074facaded6d288f107fc
Naoyuki Ishida <39.ishida@gmail.com>
2022-07-17 10:22:30 -0700

    Implement validation for area and its model validation spec

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -3,5 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Area, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    area = FactoryBot.build(:area)
+    expect(area).to be_valid
+  end
+
+  it 'is invalid without name' do
+    area = FactoryBot.build(:area, name: nil)
+    area.valid?
+    expect(area.errors[:name]).to include("can't be blank")
+  end
 end
