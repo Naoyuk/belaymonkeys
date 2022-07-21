cec699acb0677ecfe7f952558e564432386f7e78
Naoyuki Ishida <39.ishida@gmail.com>
2022-07-20 22:14:27 -0700

    Fixed devise sign up with rails7(Hotwire)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+    :confirmations => 'users/confirmations',
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
+  }
   resources :areas
   get 'static_pages/home'
   get 'static_pages/about'
   resources :messages
   resources :chats
-  devise_for :users
   resources :posts
 
   root 'posts#index'
